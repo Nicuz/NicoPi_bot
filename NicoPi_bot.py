a92816b0e2ec12bc75204d7eaefde51dce4ce622
Nicuz <nico.majorana@gmail.com>
2016-12-09 13:56:24 +0100

    creata funzione per il comando temp

--- NicoPi_bot.py
+++ NicoPi_bot.py
@@ -6,22 +6,25 @@ def handle(msg):
         command = msg['text'] #prelevo testo del messaggio
         username = msg['chat']['username'] #prelevo username del mittente
 
+        def temperatura():
+                sensor = Adafruit_DHT.DHT11
+                pin=4
+                humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+                if humidity is not None and temperature is not None:
+                        bot.sendMessage(chat_id, ("Temperatura: {0:0.1f}*C\nUmidita': {1:0.1f}%".format(temperature, humidity)))
+
         #Log su shell
         print 'Ricevuto comando: %s' % command,"da:", username
         if username == "SOSTITUIRE COL PROPRIO USERNAME SENZA @":
                 if command == '/temp':
-                        sensor = Adafruit_DHT.DHT11 #imposto il tipo di sensore
-                        pin=4 #prelevo i dati dalla GPIO4
-                        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin) #salvo i valori di umidita' e temperatura
-                        if humidity is not None and temperature is not None: #se i dati sono validi li stampo con tipo float
-                                bot.sendMessage(chat_id, ("Temperatura: {0:0.1f}*C\nUmidita': {1:0.1f}%\n".format(temperature, humidity)))
+                        temperatura()
         else:
                 bot.sendMessage(chat_id, ("E tu chi cazzo sei? Non sei autorizzato ad eseguire comandi."))
 
 TOKEN = "SOSTITUIRE COL PROPRIO TOKEN" #token del bot preso da BotFather
 bot = telepot.Bot(TOKEN)
 bot.message_loop(handle)
-print ('Bot avviato!')
+print 'Bot avviato!'
 
 while 1:
         sleep(10)
