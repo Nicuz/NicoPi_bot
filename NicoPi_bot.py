006902162555b5906bc8ac306b37131b7d1870da
Nicuz <nico.majorana@gmail.com>
2016-12-10 20:28:15 +0100

    Aggiunto utf8 e comando meteo

--- NicoPi_bot.py
+++ NicoPi_bot.py
@@ -1,5 +1,7 @@
-import telepot, Adafruit_DHT, subprocess    
+#-*- coding: utf-8 -*-
+import telepot, Adafruit_DHT, subprocess
 from time import sleep
+from pyql.weather.forecast import Forecast
 
 def on_chat_message(msg):
         chat_id = msg['chat']['id'] #prelevo id del mittente
@@ -11,20 +13,29 @@ def on_chat_message(msg):
                 pin=4
                 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
                 if humidity is not None and temperature is not None:
-                        bot.sendMessage(chat_id, ("Temperatura: {0:0.1f}*C\nUmidita': {1:0.1f}%".format(temperature, humidity)))
+                        bot.sendMessage(chat_id, ("Temperatura: {0:0.1f}*C\nUmidità: {1:0.1f}%".format(temperature, humidity)))
 
-        #da usare con if command.startswith('/COMANDO')
         def comando_shell(): #funzione che invia un messaggio contenente l'output di un comando lanciato
                 proc = subprocess.Popen([command[1:]],stdout=subprocess.PIPE, shell=True)
                 (out, err) = proc.communicate()
                 bot.sendMessage(chat_id, out)
 
+        def meteo(): #funzione che utilizza Yahoo per le informazioni meteo
+                my_woeid = 12591865 #woeid di Parma, Italy
+                forecast = Forecast.get(woeid=my_woeid, u="c")
+                city, country = forecast.location.city, forecast.location.country
+                temp, hum, cond, wind = forecast.item.condition.temp, forecast.atmosphere.humidity, forecast.item.condition.text, forecast.wind.speed
+                bot.sendMessage(chat_id, "{0}, {1}\n{2}\nTemperatura: {3}°C\nUmidità: {4}%\nVento: {5} km/h".format(city, country, cond, temp, hum, wind))
+
         #Log su shell
         print 'Ricevuto comando: %s' % command,"da:", username
 
+        #controllo sull'username
         if username == "SOSTITUIRE COL PROPRIO USERNAME SENZA @":
-                if command == '/temp':
+                if command == "/temp":
                         temperatura()
+                elif command == "/meteo":
+                        meteo()
                 else:
                         bot.sendMessage(chat_id, ("Comando non valido."))
         else:
